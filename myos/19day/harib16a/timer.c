9c2d7d0509fdc158c53ac28941864000b4715b4c
dwx748707 <dingchengfeng@huawei.com>
2019-12-04 15:48:45 +0800

    19day

---
+++ myos/19day/harib16a/timer.c
@@ -0,0 +1,119 @@
+
+
+#include "bootpack.h"
+
+#define PIT_CTRL	0x0043
+#define PIT_CNT0	0x0040
+
+struct TIMERCTL timerctl;
+
+#define TIMER_FLAGS_ALLOC		1	/* 已配置状态 */
+#define TIMER_FLAGS_USING		2	/* 定时器运行中 */
+//10ms产生一次中断信号
+void init_pit(void)
+{
+	int i;
+	struct TIMER *t;
+	io_out8(PIT_CTRL, 0x34);
+	io_out8(PIT_CNT0, 0x9c);
+	io_out8(PIT_CNT0, 0x2e);
+	timerctl.count = 0;
+	for (i = 0; i < MAX_TIMER; i++) {
+		timerctl.timers0[i].flags = 0; /* 未使用 */
+	}
+	t = timer_alloc();
+	t->timeout = 0xffffffff;
+	t->flags = TIMER_FLAGS_USING;
+	t->next = 0;
+	timerctl.t0 = t;
+	timerctl.next = 0xffffffff; /* 最初的超时时刻 */
+	return;
+}
+
+struct TIMER *timer_alloc(void)
+{
+	int i;
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timers0[i].flags == 0) {
+			timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+			return &timerctl.timers0[i];
+		}
+	}
+	return 0; /* 没有可用的定时器 */
+}
+
+void timer_free(struct TIMER *timer)
+{
+	timer->flags = 0; /* 未使用 */
+	return;
+}
+//计时器 数据队列 输出值
+void timer_init(struct TIMER *timer, struct FIFO32 *fifo, unsigned char data)
+{
+	timer->fifo = fifo;
+	timer->data = data;
+	return;
+}
+//定时器 中断次数（10ms产生一次中断信号）
+void timer_settime(struct TIMER *timer, unsigned int timeout)
+{
+	int e;
+	struct TIMER *t, *s;
+	timer->timeout = timeout + timerctl.count;
+	timer->flags = TIMER_FLAGS_USING;
+	e = io_load_eflags();
+	io_cli();
+	t = timerctl.t0;
+	if (timer->timeout <= t->timeout) {
+		/* 插入最前面 */
+		timerctl.t0 = timer;
+		timer->next = t; /* 次はt */
+		timerctl.next = timer->timeout;
+		io_store_eflags(e);
+		return;
+	}
+	/* 搜寻插入位置 */
+	for (;;) {
+		s = t;
+		t = t->next;
+		if (timer->timeout <= t->timeout) {
+			/* 插入s、t之间 */
+			s->next = timer; 
+			timer->next = t; 
+			io_store_eflags(e);
+			return;
+		}
+	}
+}
+
+void inthandler20(int *esp)
+{
+	struct TIMER *timer;
+	char ts = 0;
+	io_out8(PIC0_OCW2, 0x60);	/* 把IRQ-00信号接收完了的信息通知给PIC */
+	timerctl.count++;
+	if (timerctl.next > timerctl.count) {
+		return; /* 还没到下一个超时时刻，结束 */
+	}
+	timer = timerctl.t0;	//首先把最前面的地址赋给timer
+	for (;;) {
+		/* 找到最近的还没超时的 */
+		if (timer->timeout > timerctl.count) {
+			break;
+		} 
+		/* 超时的 */
+		timer->flags = TIMER_FLAGS_ALLOC;
+		if (timer != task_timer){
+			fifo32_put(timer->fifo, timer->data);
+		} else {
+			ts = 1; //task_timer超时
+		}
+		timer = timer->next;
+	}
+	timerctl.t0 = timer;
+	timerctl.next = timer->timeout;
+	if(ts != 0){
+		task_switch();//任务切换回修改IF的值，使能中断，所以在中断处理程序的最后，在进行任务切换
+	}
+	return;
+}
